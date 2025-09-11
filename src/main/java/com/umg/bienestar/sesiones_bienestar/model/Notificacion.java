d0376c2b6b99756ce8b24567413c8507096c6079
Amada07 <amadacarcamo938@gmail.com>
2025-09-10 22:17:55 -0600

    Notificacion
    Implementa entidad Notificación asociada a Cliente

---
+++ src/main/java/com/umg/bienestar/sesiones_bienestar/model/Notificacion.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.umg.bienestar.sesiones_bienestar.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author amada
+ */
+@Entity
+public class Notificacion {
+  
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String tipo; //confirmacion, rechazo,recordatorio,factura
+    private String titulo;
+    private String mensaje;
+    private LocalDateTime fechaHora;
+    
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+    
+    //Getters y Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(LocalDateTime fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+    
+    
+}
