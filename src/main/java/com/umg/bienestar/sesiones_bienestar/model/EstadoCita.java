80285ab2f26b547f600c72cbbf6322650c217e44
Amada07 <amadacarcamo938@gmail.com>
2025-09-10 22:13:58 -0600

    Estado Cita
    Se añadió enum con valores Pendiente,Confirmada, Cancelada ,Atendida

---
+++ src/main/java/com/umg/bienestar/sesiones_bienestar/model/EstadoCita.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.umg.bienestar.sesiones_bienestar.model;
+
+/**
+ *
+ * @author amada
+ */
+public enum EstadoCita {
+    PENDIENTE,
+    CONFIRMADA,
+    CANCELADA,
+    ATENDIDA,
+    
+}
