bcdd775ac312e32c782e04cab7ff602a5e9bd49f
Amada07 <amadacarcamo938@gmail.com>
2025-09-10 19:14:28 -0600

    Proyecto inical en Spring Boot

---
+++ src/main/java/com/umg/bienestar/sesiones_bienestar/SesionesBienestarApplication.java
@@ -0,0 +1,13 @@
+package com.umg.bienestar.sesiones_bienestar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesionesBienestarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesionesBienestarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umg/bienestar/sesiones_bienestar/SesionesBienestarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umg.bienestar.sesiones_bienestar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesionesBienestarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
