a758bf61b9dfc4e5560d856a6c1be5e2b687d997
Amada07 <amadacarcamo938@gmail.com>
2025-10-02 02:25:14 -0600

    NotificacionRepository.java - repository/jpa/
    Repositorio de notificaciones con filtros por usuario y tipo

--- src/main/java/com/umg/bienestar/sesiones_bienestar/repository/jpa/NotificacionRepository.java
+++ src/main/java/com/umg/bienestar/sesiones_bienestar/repository/jpa/NotificacionRepository.java
@@ -5,12 +5,21 @@
 package com.umg.bienestar.sesiones_bienestar.repository.jpa;
 
 import com.umg.bienestar.sesiones_bienestar.entity.Notificacion;
+import com.umg.bienestar.sesiones_bienestar.entity.TipoNotificacion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
 /**
  *
  * @author amada
  */
-public interface NotificacionRepository extends JpaRepository<Notificacion,Long>{
-    
+
+@Repository
+public interface NotificacionRepository extends JpaRepository<Notificacion, Long> {
+    List<Notificacion> findByUsuarioId(Long usuarioId);
+    List<Notificacion> findByUsuarioIdAndLeidaFalse(Long usuarioId);
+    List<Notificacion> findByTipo(TipoNotificacion tipo);
+    List<Notificacion> findByEnviadaFalse();
 }
+
