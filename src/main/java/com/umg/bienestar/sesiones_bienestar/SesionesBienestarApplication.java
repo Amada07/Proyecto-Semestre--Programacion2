7efb2dcf1f845e571436da8d01548dd2e14077c2
Amada07 <amadacarcamo938@gmail.com>
2025-10-02 02:36:43 -0600

    SesionesBienestarApplication.java - raíz
    Clase principal de Spring Boot con JPA Auditing habilitado

--- src/main/java/com/umg/bienestar/sesiones_bienestar/SesionesBienestarApplication.java
+++ src/main/java/com/umg/bienestar/sesiones_bienestar/SesionesBienestarApplication.java
@@ -2,8 +2,15 @@ package com.umg.bienestar.sesiones_bienestar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ *
+ * @author amada
+ */
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SesionesBienestarApplication {
 
 	public static void main(String[] args) {
