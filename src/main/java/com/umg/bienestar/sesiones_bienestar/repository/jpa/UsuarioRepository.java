c29c841886b33b29d6f3113e57e752d4d174f9ce
Amada07 <amadacarcamo938@gmail.com>
2025-10-02 02:26:54 -0600

    UsuarioRepository.java - repository/jpa/
    Repositorio base de usuarios con autenticación

---
+++ src/main/java/com/umg/bienestar/sesiones_bienestar/repository/jpa/UsuarioRepository.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.umg.bienestar.sesiones_bienestar.repository.jpa;
+
+import com.umg.bienestar.sesiones_bienestar.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+/**
+ *
+ * @author amada
+ */
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
+    Optional<Usuario> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}
