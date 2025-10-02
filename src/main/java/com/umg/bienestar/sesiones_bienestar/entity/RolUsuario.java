e0869fe9fc8c5fd4465f84338ae92606c3559cc0
Amada07 <amadacarcamo938@gmail.com>
2025-10-02 01:37:34 -0600

    RolUsuario-entity/
    Enum de roles del sistema

---
+++ src/main/java/com/umg/bienestar/sesiones_bienestar/entity/RolUsuario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.umg.bienestar.sesiones_bienestar.entity;
+
+/**
+ *
+ * @author amada
+ */
+public enum RolUsuario {
+  ADMINISTRADOR,
+  RECEPCIONISTA,
+  CLIENTE,
+  TERAPEUTA,
+}
